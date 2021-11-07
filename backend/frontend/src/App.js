118b62e8d9a69d60588c49c8834a11753b5b3f93
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-11-07 21:11:35 +0530

    database update successful

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Background from "./components/Background";
-// import Home from "./components/Home";
-import AddBook from "./components/AddBook";
+import Home from "./components/Home";
+// import AddBook from "./components/AddBook";
 
 function App() {
   return (
     <>
       <Background></Background>
       <Navbar></Navbar>
-      {/* <Home></Home> */}
-      <AddBook></AddBook>
+      <Home></Home>
+      {/* <AddBook></AddBook> */}
     </>
   );
 }

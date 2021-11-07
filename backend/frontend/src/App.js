dbc2957f68dc5f07ffe8789ca9a9d2f09ea00bbd
Abhijeet Shankar Mishra <abhijeet.s.mishra@gmail.com>
2021-11-07 20:01:23 +0530

    Home page update

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Background from "./components/Background";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Background></Background>
       <Navbar></Navbar>
+      <Home></Home>
     </>
   );
 }
---
+++ backend/frontend/src/components/Home.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="card container">
+      <div className="card-body">
+        <h4>What is a Library Management System ?</h4>
+        <section>
+          A Library Management System is a software built to handle the primary
+          housekeeping functions of a library. Libraries rely on library
+          management systems to manage asset collections as well as
+          relationships with their members. Library management systems help
+          libraries keep track of the books and their checkouts, as well as
+          members’ subscriptions and profiles. Library management systems also
+          involve maintaining the database for entering new books and recording
+          books that have been borrowed with their respective due dates.
+        </section>
+        <hr />
+        <h4>Importance of LMS</h4>
+        <section>
+          <ul>
+            <li>
+              A library management system is the most proficient and easy to use
+              system for managing all the processes involved in a Library in the
+              most effective ways.
+            </li>
+            <li>
+              This system will reduce all the manual work and the whole process
+              can be managed just through single clicks and edits.
+            </li>
+            <li>
+              There will be no headache and doubtfulness of storing the data
+              securely and searching the records of any individual afterward.
+            </li>
+            <li>
+              Any book seeker can rent a book just by signing in with their
+              details, and return it with the date of returning.
+            </li>
+            <li>
+              The staff can also facilitate themselves with some extra
+              authorizations and privileges. Only, one person is required to
+              take care of the whole system, without any chances of mistakes.
+            </li>
+          </ul>
+        </section>
+        <hr />
+        <h4> Key Features</h4>
+        <section>
+          A library Management System does the following tasks :
+          <br />
+          <ul>
+            <li>
+              Maintaining stock : This is a very important feature of all
+              library software. With the help of library system we can maintain
+              the stocks such as books,newspapers, magazines more efficiently.
+              With the help of this feature our manual works have reduced to a
+              great extent. This feature can also be termed as cataloging. This
+              feature also helps in editing or deleting the data, if required.
+            </li>
+            <li>
+              Classification of stocks : The stocks can be classified as per
+              author name, publication, category, price , location of book (i.e
+              shelf or rack id).
+            </li>
+            <li>
+              Search : The LMS provides search option to the librarians which
+              makes their work fast and easy. Librarian can search stock in
+              seconds.
+            </li>
+            <li>
+              Report : LMS audits the entire library in no time and generate
+              reports accordingly, which makes the work of a librarian very
+              easy.
+            </li>
+            <li>
+              Manages demands : It keeps records of the demand by the members of
+              those item that are currently unavailable.
+            </li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+}

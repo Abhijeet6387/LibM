9eaca497612f3c89d8c5310bbbb1fc10a63a9279
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2022-01-26 19:11:41 +0530

    login and register working

--- backend/frontend/src/components/Login.js
+++ backend/frontend/src/components/Login.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { validateEmail } from "../helpers/validation";
 export default function Login() {
+  const [user, setUser] = useState({ email: "", password: "" });
+  const onChangeUser = (e) => {
+    //  spread operator ...
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (!validateEmail(user.email)) alert("Enter the valid email");
+    else if (user.password === "") alert("Password cannot be empty");
+    else {
+      axios
+        .post("/users/login", user)
+        .then((res) => {
+          alert(res.data.message);
+          console.log(res.data);
+          localStorage.setItem("my_token", res.data.token);
+          window.location.href = "/home";
+        })
+        .catch((err) => {
+          alert(err.message);
+          console.log(err);
+        });
+    }
+  };
   return (
     <div className="card container cardhw">
       <div className="card-body">
         <h4 className="text-center">Login</h4>
-        <form autocomplete="off">
+        <form autocomplete="off" onSubmit={onSubmitForm}>
           <div className="form-group">
             <label>Email</label>
             <input
               type="email"
               className="form-control"
               placeholder="Enter Email"
+              value={user.email}
+              name="email"
+              onChange={onChangeUser}
             />
           </div>
           <div className="form-group">
@@ -21,6 +49,9 @@ export default function Login() {
               className="form-control"
               type="password"
               placeholder="Enter Password"
+              name="password"
+              value={user.password}
+              onChange={onChangeUser}
               required
             />
           </div>
--- backend/frontend/src/components/Register.js
+++ backend/frontend/src/components/Register.js
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [isPatron, setIsPatron] = useState(true);
+  const [contact, setContact] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const onChangePass = (e) => {
+    setPass(e.target.value);
+  };
+  const onChangeIsPatron = (e) => {
+    setIsPatron(e.target.value);
+  };
+  const onChangeContact = (e) => {
+    setContact(e.target.value);
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (name === "" || email === "" || pass === "" || contact === "") {
+      alert("Please fill the details");
+    } else {
+      axios
+        .post("/users/register", {
+          name: name,
+          email: email,
+          password: pass,
+          isPatron: isPatron,
+          contact: contact,
+        })
+        .then((res) => {
+          alert("User added successfully");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    setName("");
+    setEmail("");
+    setPass("");
+    setIsPatron(true);
+    setContact("");
+  };
   return (
     <div className="card container cardhw">
       <div className="card-body">
         <h4 className="text-center">Register </h4>
-        <form autocomplete="off">
+        <form autocomplete="off" onSubmit={onSubmitForm}>
           <div className="form-group">
             <label>Name</label>
             <input
               type="text"
               className="form-control"
               placeholder="Enter Name"
+              value={name}
+              onChange={onChangeName}
             />
           </div>
           <div className="form-group">
@@ -21,6 +71,8 @@ export default function Register() {
               className="form-control"
               type="email"
               placeholder="Enter Email"
+              value={email}
+              onChange={onChangeEmail}
               required
             />
           </div>
@@ -31,20 +83,19 @@ export default function Register() {
               type="password"
               placeholder="Enter Password"
               id="password"
+              value={pass}
+              onChange={onChangePass}
               required
             />
           </div>
           <div className="form-check">
-            <label
-              className="form-check-label float-right"
-              for="flexCheckDefault"
-            >
+            <label className="form-check-label float-right" for="checkpatron">
               <input
                 className="form-check-input"
                 type="checkbox"
-                value=""
-                id="flexCheckDefault"
-                defaultChecked="true"
+                value={isPatron}
+                onChange={onChangeIsPatron}
+                id="checkpatron"
               />
               Are you a Patron ?
             </label>
@@ -55,6 +106,8 @@ export default function Register() {
               className="form-control"
               type="text"
               placeholder="Enter Contact"
+              value={contact}
+              onChange={onChangeContact}
               required
             />
           </div>

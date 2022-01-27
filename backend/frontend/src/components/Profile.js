d7c5019c3aca3ce29ba24895f4bcc92b720319f2
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2022-01-27 12:13:42 +0530

    profile login and register user completed

--- backend/frontend/src/components/Profile.js
+++ backend/frontend/src/components/Profile.js
@@ -1,18 +1,91 @@
 import React from "react";
-
+import user from "../images/user2.png";
 export default function Home(props) {
+  const handleLogout = () => {
+    localStorage.removeItem("my_token");
+    window.location.href = "/home";
+  };
   return (
     <div className="card container">
-      <div className="card-body">
-        <h4>Welcome to LibM, User!</h4>
-        {props.userInfo && (
-          <ul>
-            <li>Name: {props.userInfo.name} </li>
-            <li>Email:{props.userInfo.email}</li>
-            <li>Role: {props.userInfo.isPatron ? "Patron" : "Librarian"}</li>
-            <li>Contact: {props.userInfo.contact} </li>
-          </ul>
-        )}
+      <h4 className="pt-4">Welcome to LibM, User!</h4>
+      <div className="card-body row">
+        <div className="col-sm-4">
+          <img
+            className="card-img-top"
+            src={user}
+            alt="user"
+            style={{
+              height: "200px",
+              width: "auto",
+              marginBottom: "10px",
+              marginLeft: "25%",
+            }}
+          />
+        </div>
+        <div className="col-sm-8">
+          <div className="row">
+            <div className="col">
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item" style={{ fontWeight: "bold" }}>
+                  Name
+                </li>
+                <li className="list-group-item" style={{ fontWeight: "bold" }}>
+                  Email
+                </li>
+                <li className="list-group-item" style={{ fontWeight: "bold" }}>
+                  Role
+                </li>
+                <li className="list-group-item" style={{ fontWeight: "bold" }}>
+                  Contact
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              {props.userInfo && (
+                <ul className="list-group list-group-flush">
+                  <li
+                    className="list-group-item"
+                    style={{ fontWeight: "bold" }}
+                  >
+                    {" "}
+                    {props.userInfo.name}
+                  </li>
+                  <li
+                    className="list-group-item"
+                    style={{ fontWeight: "bold" }}
+                  >
+                    {" "}
+                    {props.userInfo.email}
+                  </li>
+                  <li
+                    className="list-group-item"
+                    style={{ fontWeight: "bold" }}
+                  >
+                    {" "}
+                    {props.userInfo.isPatron ? "Patron" : "Librarian"}
+                  </li>
+                  <li
+                    className="list-group-item"
+                    style={{ fontWeight: "bold" }}
+                  >
+                    {" "}
+                    {props.userInfo.contact}
+                  </li>
+                </ul>
+              )}
+            </div>
+          </div>
+          <div className="row ml-3 mt-3">
+            <button
+              className="btn btn-outline-success btn-block"
+              type="button"
+              style={{ borderRadius: "20px", borderColor: "green" }}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
